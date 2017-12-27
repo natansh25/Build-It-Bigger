bc77c20efe65982ed52a843a4be8f1fb7ac79e77
Natansh <natansh25@gmail.com>
2017-12-27 12:53:36 -0500

    Initial commit

---
+++ androidjokeslibrary/src/main/java/com/example/androidjokeslibrary/AndroidLibActivity.java
@@ -0,0 +1,20 @@
+package com.example.androidjokeslibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AndroidLibActivity extends AppCompatActivity {
+
+    TextView txt_joke;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_lib);
+
+        txt_joke=findViewById(R.id.txt_lib);
+
+
+    }
+}
---
+++ jokes/src/main/java/com/example/jokes/Joke.java
@@ -0,0 +1,11 @@
+package com.example.jokes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Joke {
+
+    public String MyName = "Natansh Pandya";
+
+
+}
